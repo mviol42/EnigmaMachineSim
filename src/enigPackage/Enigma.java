b70bc22333b898487bf254333300ff3d18dad80c
Sentrai <mattmho@uw.edu>
2020-12-09 18:20:03 -0800

    Added ALPHABET_SIZE, removed the setter for the number of rotors, and re-ordered a few bits of code

--- src/enigPackage/Enigma.java
+++ src/enigPackage/Enigma.java
@@ -17,20 +17,19 @@ public class Enigma {
 	private Rotor entryWheel;
 
 	// Creates a new Enigma for the given, user-decided rotors, the name of the .txt file with the  input phrase, and
-	// the reflector and entry wheel rotors.
+	// 		the reflector and entry wheel rotors.
 	public Enigma(RotorList chosenRotors, String txtFileName, Rotor reflector, Rotor entryWheel) throws FileNotFoundException {
 		this.firstRotor = chosenRotors;
 		this.phrase = TextConvertor.convertToQueue(txtFileName);
 		this.reflector = reflector;
 		this.entryWheel = entryWheel;
-		System.out.println(this.phrase);
 	}
 
 	// Returns the encrypted cipher from the phrase as a character array. All spaces are removed and the entire array
-	// is lowercase.
+	//     	is lowercase.
 	// Specifically, this method removes the current character currChar from the queue, runs it through entryWheel,
-	// runs it through the recursive helper method which passes the character through the 3 main rotors and reflector,
-	// and then runs it back out entryWheel. The character is now encrypted and is added to cipher.
+	//     	runs it through the recursive helper method which passes the character through the 3 main rotors and reflector,
+	//     	and then runs it back out entryWheel. The character is now encrypted and is added to cipher.
 	public char[] encryptor() {
 		char[] cipher = new char[this.phrase.size()];
 		int counter = 0;
@@ -45,14 +44,13 @@ public class Enigma {
 		return cipher;
 	}
 
-	// A helper method for encryptor that recursively passes a character currChar through all the rotors and reflector
-	// of a given Enigma and handles rotor position rotations for all 3 main rotors.
+	// A helper method for encryptor that recursively passes a character currChar through all the 
+	//		rotors and reflector of a given Enigma and handles rotor position rotations for all 
+	// 		3 main rotors.
 	private char encryptorHelper(char currChar, RotorList currRotorList) {
 		if (currRotorList.previous == null) {
-			System.out.println("rotated first rotor to" + currRotorList.rotor.getPosition());
 			currRotorList.rotor.rotate();
 		} else if (currRotorList.previous.rotor.getPosition() == 0 && currRotorList.previous.rotor.getJustRotated()) {
-			System.out.println("other rotor rotated");
 			currRotorList.rotor.rotate();
 			currRotorList.previous.rotor.setJustRotated(false);
 		}
